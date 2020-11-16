d8127bb7f9edcae93631783fadf1e37a84f56472
Polina <polina_sch@web.de>
2020-11-16 16:42:57 +0100

    edit home view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,63 @@
 <template>
-    <div id="home">
-        <h1>Home</h1>
-        <p>
-            Test
-        </p>
-    </div>
+  <div id="home">
+    <b-container class="home-container" fluid>
+      <b-row>
+        <b-col>
+            <b-button type="button" block variant="light" class="btn">Verbleibende Urlaubstage</b-button>
+        </b-col>
+        <b-col>
+            <b-button type="button" block variant="light" class="btn">In Bearbeitung</b-button>
+        </b-col>
+        <b-col>
+            <b-button type="button" block variant="light" class="btn">Ausstehende Genehmigungen</b-button>
+        </b-col>
+        <b-col>
+            <b-button type="button" block variant="light" class="btn">Frei planbar</b-button>
+        </b-col>
+      </b-row>
+      <b-row>
+        <b-col>
+            <b-button type="button" block variant="dark" class="btn">Benutzerverwaltung</b-button>
+        </b-col>
+        <b-col>
+            <b-button type="button" block variant="dark" class="btn">Arbeitstage konfigurieren</b-button>
+        </b-col>
+      </b-row>
+      <b-row>
+        <b-col>
+            <b-button type="button" block variant="dark" class="btn">Erhaltene Urlaubsanträge</b-button>
+        </b-col>
+        <b-col>
+            <b-button type="button" block variant="dark" class="btn">Urlaubsliste</b-button>
+        </b-col>
+      </b-row>
+      <b-row>
+        <b-col>
+            <router-link to="/urlaubsantrag">
+            <b-button type="button" block variant="dark" class="btn">Urlaubsantrag erstellen</b-button>
+            </router-link>
+        </b-col>
+        <b-col>
+            <b-button type="button" block variant="dark" class="btn"> Meine Urlaubsanträge</b-button>
+        </b-col>
+      </b-row>
+    </b-container>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: 'Home',
-        data() {
-            return {};
-        }
-    }
+export default {
+  name: "Home",
+  data() {
+    return {};
+  },
+  methods: {
+  }
+};
 </script>
+
+<style>
+   .btn {
+        margin: 5px;
+    }  
+</style>
