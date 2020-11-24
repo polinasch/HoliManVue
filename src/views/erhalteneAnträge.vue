295c4e2a35e34ff755af73cf143018454ae6b11c
Polina <polina_sch@web.de>
2020-11-24 21:33:15 +0100

    cancel button, edit fields and add data

--- src/views/erhalteneAnträge.vue
+++ src/views/erhalteneAnträge.vue
@@ -1,16 +1,21 @@
 <template>
   <b-container>
-    <b-table striped bordered hover :fields="fields" :sort-desc.sync="sortDesc"></b-table>
+    <b-table striped bordered hover :fields="fields" :items="erhalteneAnträge" :sort-desc.sync="sortDesc"></b-table>
+    <router-link to="/home">
+    <b-button type="cancel" block variant="light">Abbrechen</b-button>
+    </router-link>
   </b-container>
 </template>
 
 <script>
+import axios from "axios";
+import { server } from "../helper.js";
   export default {
     name: 'erhalteneAnträge',
     data() {
       return {
         fields: [
-            {key: 'ID', sortable: true},
+            {key: 'AntragID', sortable: true},
             {key: 'Vorname', sortable: true},
             {key: 'Nachname', sortable: true}, 
             {key: 'Urlaubsart', sortable: true},
@@ -18,7 +23,17 @@
             {key: 'von', sortable: true},
             {key: 'bis', sortable: true},
             {key: ' ', sortable: false}
-            ],
+        ],
+        erhalteneAnträge: []
+      }
+    },
+    created() {
+      this.getErhalteneAnträge();
+    },
+    methods: {
+      getErhalteneAnträge(){
+        axios.get(server.baseURL + '/urlaubsantrag').then(response =>
+        (this.erhalteneAnträge = response.data));
       }
     }
   }
