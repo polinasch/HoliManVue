e8f6f733a583b5915aa5f7d0ca291202dfe13661
Polina <polina_sch@web.de>
2020-11-25 21:01:55 +0100

    logout button

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,44 @@
 <template>
-    <div id="app">
-        <!--<Login/>-->
-        <router-link v-if="authenticated" to="/login" v-on:click.native="logout()" replace>Logout</router-link>
-        <router-view @authenticated="setAuthenticated" />
-    </div>
+  <div id="app">
+    <!--<Login/>-->
+    <router-link
+      v-if="authenticated"
+      to="/login"
+      v-on:click.native="logout()"
+      replace
+    >
+      <b-button variant="outline-primary" class="mb-2">
+        <b-icon icon="power" aria-hidden="true"></b-icon> Logout
+      </b-button>
+    </router-link>
+    <router-view @authenticated="setAuthenticated" />
+  </div>
 </template>
 
 <script>
 export default {
-    name: "App",
-    data() {
-            return {
-                authenticated: false,
-                mockAccount: {
-                    username: "admin",
-                    password: "test"
-                }
-            }
+  name: "App",
+  data() {
+    return {
+      authenticated: false,
+      mockAccount: {
+        username: "admin",
+        password: "test",
+      },
+    };
+  },
+  mounted() {
+    if (!this.authenticated) {
+      this.$router.replace({ name: "login" });
+    }
+  },
+  methods: {
+    setAuthenticated(status) {
+      this.authenticated = status;
     },
-    mounted() {
-            if(!this.authenticated) {
-                this.$router.replace({ name: "login" });
-            }
-        },
-    methods: {
-            setAuthenticated(status) {
-                this.authenticated = status;
-            },
-            logout() {
-                this.authenticated = false;
-            }
-        }
-}
+    logout() {
+      this.authenticated = false;
+    },
+  },
+};
 </script>
