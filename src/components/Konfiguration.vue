e4d35bcc10551bd2f80ec4258e6d77308aa13b15
Polina <polina_sch@web.de>
2020-11-27 16:34:22 +0100

    create single checkboxes

--- src/components/Konfiguration.vue
+++ src/components/Konfiguration.vue
@@ -3,12 +3,28 @@
     <h2>Arbeitstage konfigurieren</h2>
     <b-form @reset="onReset" @submit="createKonfiguration">
       <b-form-group label="Wählen Sie die Arbeitstage aus:">
-        <b-form-checkbox-group
-          v-model="arbeitstage"
-          :options="auswahl"
-          stacked
-        ></b-form-checkbox-group>
-      </b-form-group>
+        <b-form-checkbox id="input-1" v-model="Montag"
+          >Montag</b-form-checkbox
+        >
+        <b-form-checkbox id="input-2" v-model="Dienstag"
+          >Dienstag</b-form-checkbox
+        >
+        <b-form-checkbox id="input-3" v-model="Mittwoch"
+          >Mittwoch</b-form-checkbox
+        >
+        <b-form-checkbox id="input-4" v-model="Donnerstag"
+          >Donnerstag</b-form-checkbox
+        >
+        <b-form-checkbox id="input-5" v-model="Freitag"
+          >Freitag</b-form-checkbox
+        >
+        <b-form-checkbox id="input-6" v-model="Samstag"
+          >Samstag</b-form-checkbox
+        >
+        <b-form-checkbox id="input-6" v-model="Sonntag"
+          >Sonntag</b-form-checkbox
+        >
+        </b-form-group>
       <b-button type="submit" variant="primary">Speichern</b-button>
       <b-button type="reset" variant="secondary">Abbrechen</b-button>
     </b-form>
@@ -22,33 +38,36 @@ export default {
   name: "Konfiguration",
   data() {
     return {
-      arbeitstage: [],
-      auswahl: [
-        { text: "Montag", value: "Montag" },
-        { text: "Dienstag", value: "Dienstag" },
-        { text: "Mittwoch", value: "Mittwoch" },
-        { text: "Donnerstag", value: "Donnerstag" },
-        { text: "Freitag", value: "Freitag" },
-        { text: "Samstag", value: "Samstag" },
-        { text: "Sonntag", value: "Sonntag" },
-      ],
+      Montag: false,
+      Dienstag: false,
+      Mittwoch: false,
+      Donnerstag: false,
+      Freitag: false,
+      Samstag: false,
+      Sonntag: false
     };
   },
   methods: {
     onReset(evt) {
       evt.preventDefault();
-        this.arbeitstage = [];
+        this.Montag = false;
+        this.Dienstag = false;
+        this.Mittwoch = false;
+        this.Donnerstag = false;
+        this.Freitag = false;
+        this.Samstag = false;
+        this.Sonntag = false;
         this.$router.push({ name: "home" });
     },
     createKonfiguration(){
       let konfiguration = {
-        Montag: this.arbeitstage.Montag,
-        Dienstag: this.arbeitstage.Dienstag,
-        Mittwoch: this.arbeitstage.Mittwoch,
-        Donnerstag: this.arbeitstage.Donnerstag,
-        Freitag: this.arbeitstage.Freitag,
-        Samstag: this.arbeitstage.Samstag,
-        Sonntag: this.arbeitstage.Sonntag
+        Montag: this.Montag,
+        Dienstag: this.Dienstag,
+        Mittwoch: this.Mittwoch,
+        Donnerstag: this.Donnerstag,
+        Freitag: this.Freitag,
+        Samstag: this.Samstag,
+        Sonntag: this.Sonntag
       }
       this.submitToServer(konfiguration);
     },
