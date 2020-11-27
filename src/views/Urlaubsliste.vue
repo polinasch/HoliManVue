55525b37280411ef6f78013a78fee8651d7ddee0
Polina <polina_sch@web.de>
2020-11-27 20:53:45 +0100

    edit table

--- src/views/Urlaubsliste.vue
+++ src/views/Urlaubsliste.vue
@@ -1,6 +1,25 @@
 <template>
   <b-container>
-    <b-table striped bordered hover :fields="fields" :items="urlaubsliste" :sort-desc.sync="sortDesc"></b-table>
+    <b-table striped bordered hover :fields="fields" :items="urlaubsliste" :sort-desc.sync="sortDesc">
+      <template #cell(BenutzerID)="row">
+        {{ row.item.benutzer.BenutzerID }}
+      </template>
+      <template #cell(Nachname)="row">
+        {{ row.item.benutzer.Nachname }}
+      </template>
+      <template #cell(Vorname)="row">
+        {{ row.item.benutzer.Vorname }}
+      </template>
+      <template #cell(urlaubstage)="row">
+        {{ row.item.von }} bis {{row.item.bis}}
+      </template>
+      <template #cell(von)="row">
+        {{ row.item.von }}
+      </template>
+      <template #cell(bis)="row">
+        {{row.item.bis}}
+      </template>
+    </b-table>
     <router-link to="/home">
     <b-button type="cancel" block variant="light">Abbrechen</b-button>
     </router-link>
@@ -14,14 +33,14 @@ import { server } from "../helper.js";
     name: 'Urlaubsliste',
     data() {
       return {
+        sortDesc: false,
         fields: [
             {key: 'BenutzerID', sortable: true},
             {key: 'Nachname', sortable: true},
             {key: 'Vorname', sortable: true}, 
             {key: 'Urlaubstage', sortable: true}, 
             {key: 'von', sortable: true},
-            {key: 'bis', sortable: true},
-            {key: ' ', sortable: false}
+            {key: 'bis', sortable: true}
         ],
         urlaubsliste: []
       }
