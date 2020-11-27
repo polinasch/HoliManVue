588054b6f085ddc7ea045599732a928746088ae1
Polina <polina_sch@web.de>
2020-11-27 20:53:16 +0100

    edit table

--- src/views/erhalteneAnträge.vue
+++ src/views/erhalteneAnträge.vue
@@ -1,6 +1,39 @@
 <template>
   <b-container>
-    <b-table striped bordered hover :fields="fields" :items="erhalteneAnträge" :sort-desc.sync="sortDesc"></b-table>
+    <b-table striped bordered hover :fields="fields" :items="erhalteneAnträge" :sort-desc.sync="sortDesc">
+      <template #cell(AntragID)="row">
+        {{ row.item.AntragID }}
+      </template>
+      <template #cell(Benutzername)="row">
+        {{ row.item.benutzer.Benutzername }}
+      </template>
+      <template #cell(Vorname)="row">
+        {{ row.item.benutzer.Vorname }}
+      </template>
+      <template #cell(Nachname)="row">
+        {{ row.item.benutzer.Nachname }}
+      </template>
+      <template #cell(Urlaubsart)="row">
+        {{ row.item.Urlaubsart }}
+      </template>
+      <template #cell(Urlaubstage)="row">
+        {{ row.item.von }} bis {{row.item.bis}}
+      </template>
+      <template #cell(von)="row">
+        {{ row.item.von }}
+      </template>
+      <template #cell(bis)="row">
+        {{ row.item.bis }}
+      </template>
+      <template #cell(Aktionen)="row">
+        <b-button size="sm" @click="editAntrag(row.item.AntragID)" variant="primary">
+          <b-icon icon="pencil-fill"></b-icon>
+        </b-button>
+        <b-button size="sm" @click="deleteAntrag(row.item.AntragID)" variant="danger">
+          <b-icon icon="trash-fill"></b-icon>
+        </b-button>
+      </template>
+    </b-table>
     <router-link to="/home">
     <b-button type="cancel" block variant="light">Abbrechen</b-button>
     </router-link>
@@ -14,6 +47,7 @@ import { server } from "../helper.js";
     name: 'erhalteneAnträge',
     data() {
       return {
+        sortDesc: false,
         fields: [
             {key: 'AntragID', sortable: true},
             {key: 'Vorname', sortable: true},
@@ -22,7 +56,7 @@ import { server } from "../helper.js";
             {key: 'Urlaubstage', sortable: true},
             {key: 'von', sortable: true},
             {key: 'bis', sortable: true},
-            {key: ' ', sortable: false}
+            {key: 'Aktionen', sortable: false}
         ],
         erhalteneAnträge: []
       }
@@ -34,6 +68,13 @@ import { server } from "../helper.js";
       getErhalteneAnträge(){
         axios.get(server.baseURL + '/urlaubsantrag').then(response =>
         (this.erhalteneAnträge = response.data));
+      },
+      deleteAntrag(id){
+        axios.delete(server.baseURL + '/urlaubsantrag/' + id).then(response =>{
+          console.log(response);
+          this.getErhalteneAnträge();
+        }
+        )
       }
     }
   }
