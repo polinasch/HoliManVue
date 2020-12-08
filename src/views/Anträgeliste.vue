1de1af8a9c554f2ed12c58e157307fc987fea174
Polina <polina_sch@web.de>
2020-12-08 18:16:10 +0100

    get Anträge by BenutzerID

--- src/views/Anträgeliste.vue
+++ src/views/Anträgeliste.vue
@@ -1,41 +1,80 @@
 <template>
   <b-container>
-    <h2>Meine Urlaubsanträge</h2>
-    <b-table striped bordered hover :fields="fields" :items="alleAnträge" :sort-desc.sync="sortDesc">
+    <h2>Urlaubsanträge der Mitarbeiter</h2>
+    <b-form-group id="input-group-1" label="Mitarbeiter:" label-for="input-1">
+      <b-form-select id="input-1" v-model="benutzer" required @change="getAllAnträge(benutzer)">
+        <b-form-select-option
+          v-for="benutzer in Benutzer"
+          :key="benutzer.BenutzerID"
+          v-bind:value="benutzer.BenutzerID"
+        >
+          {{ "ID: " + benutzer.BenutzerID + ", " + benutzer.Vorname + " " + benutzer.Nachname }}
+        </b-form-select-option>
+      </b-form-select>
+    </b-form-group>
+    <b-table
+      striped
+      bordered
+      hover
+      :fields="fields"
+      :items="alleAnträge"
+      :sort-desc.sync="sortDesc"
+    >
       <template #cell(Urlaubsart)="row">
         {{ row.item.Urlaubsart }}
       </template>
       <template #cell(Urlaubstage)="row">
-         {{ moment(row.item.von).format('DD.MM.YYYY')}} bis {{ moment(row.item.bis).format('DD.MM.YYYY')}}
+        {{ moment(row.item.von).format("DD.MM.YYYY") }} bis
+        {{ moment(row.item.bis).format("DD.MM.YYYY") }}
       </template>
       <template #cell(von)="row">
-        {{ moment(row.item.von).format('DD.MM.YYYY')}}
+        {{ moment(row.item.von).format("DD.MM.YYYY") }}
       </template>
       <template #cell(bis)="row">
-        {{ moment(row.item.bis).format('DD.MM.YYYY')}}
+        {{ moment(row.item.bis).format("DD.MM.YYYY") }}
       </template>
       <template #cell(Status)="row">
         {{ row.item.Status }}
       </template>
       <template #cell(Aktionen)="row">
-        <router-link :to="{ name: 'editUrlaubsantrag', params: { id: row.item.AntragID } }">
-        <b-button size="sm" @click="editAntrag(row.item.AntragID)" variant="secondary">
-          <b-icon icon="pencil-fill"></b-icon>
-        </b-button>
+        <router-link
+          :to="{ name: 'editUrlaubsantrag', params: { id: row.item.AntragID } }"
+        >
+          <b-button
+            size="sm"
+            @click="editAntrag(row.item.AntragID)"
+            variant="secondary"
+          >
+            <b-icon icon="pencil-fill"></b-icon>
+          </b-button>
         </router-link>
-        <b-button size="sm" @click="sendAntrag(row.item.AntragID)" variant="primary">
+        <b-button
+          size="sm"
+          @click="sendAntrag(row.item.AntragID)"
+          variant="primary"
+        >
           <b-icon icon="envelope"></b-icon>
         </b-button>
-        <b-button size="sm" @click="cancelAntrag(row.item.AntragID)" variant="danger">
+        <b-button
+          size="sm"
+          @click="cancelAntrag(row.item.AntragID)"
+          variant="danger"
+        >
           <b-icon icon="x"></b-icon>
         </b-button>
-        <b-button size="sm" @click="deleteAntrag(row.item.AntragID)" variant="danger">
+        <b-button
+          size="sm"
+          @click="deleteAntrag(row.item.AntragID)"
+          variant="danger"
+        >
           <b-icon icon="trash-fill"></b-icon>
         </b-button>
       </template>
     </b-table>
     <router-link to="/home">
-    <b-button type="cancel" block variant="light">Zurück zur Startseite</b-button>
+      <b-button type="cancel" block variant="light"
+        >Zurück zur Startseite</b-button
+      >
     </router-link>
   </b-container>
 </template>
@@ -43,39 +82,50 @@
 <script>
 import axios from "axios";
 import { server } from "../helper.js";
-  export default {
-    name: 'Anträgeliste',
-    data() {
-      return {
-        sortDesc: false,
-        fields: [
-            {key: 'Urlaubsart', sortable: false},
-            {key: 'Urlaubstage', sortable: true},
-            {key: 'von', sortable: true}, 
-            {key: 'bis', sortable: true},
-            {key: 'Status', sortable: true},
-            {key: 'Aktionen', sortable: false}
-        ],
-        alleAnträge: []
-      }
+export default {
+  name: "Anträgeliste",
+  data() {
+    return {
+      sortDesc: false,
+      fields: [
+        { key: "Urlaubsart", sortable: false },
+        { key: "Urlaubstage", sortable: true },
+        { key: "von", sortable: true },
+        { key: "bis", sortable: true },
+        { key: "Status", sortable: true },
+        { key: "Aktionen", sortable: false },
+      ],
+      alleAnträge: [],
+      benutzer: null,
+      Benutzer: []
+    };
+  },
+  created() {
+    this.getBenutzer();
+  },
+  methods: {
+    getAllAnträge(id) {
+      axios
+        .get(server.baseURL + "/urlaubsantrag/benutzer/" + id)
+        .then((response) => (this.alleAnträge = response.data))
+        .catch(function (error){
+          console.log(error.response);
+        });
     },
-    created() {
-      this.getAllAnträge();
-    },
-    methods: {
-      getAllAnträge(){
-        axios.get(server.baseURL + '/urlaubsantrag').then(response =>
-        (this.alleAnträge = response.data));
-      },
-      deleteAntrag(id){
-        axios.delete(server.baseURL + "/urlaubsantrag/" + id).then((response) => {
+    deleteAntrag(id) {
+      axios.delete(server.baseURL + "/urlaubsantrag/" + id).then((response) => {
         console.log(response);
         this.getAllAnträge();
       });
-      },
-      editAntrag(id){
-        console.log(id);
-      }
+    },
+    editAntrag(id) {
+      console.log(id);
+    },
+    getBenutzer() {
+        axios
+        .get(server.baseURL + "/benutzer")
+        .then((response) => (this.Benutzer = response.data));
     }
-  }
+  },
+};
 </script>
